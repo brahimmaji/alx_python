0b028500e66a83e7c2155f2eebc87b2394f8af1c
brahimmaji <brahimmaji1@gmail.com>
2023-12-07 16:14:27 +0100

    reverse

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,6 +1,6 @@
+#Write a Python function called reverse_string that takes a string as input and returns the reverse of that string.
 def reverse_string(string):
-    string=input("Enter a string: ")
-    reverse_string(string)
+    string=string[::-1]
     return string
 
-print(reverse_string())
+print(reverse_string("ghnj,k;lm"))
