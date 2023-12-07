d213050837962b55b868244f1ab37583b5759059
brahimmaji <brahimmaji1@gmail.com>
2023-12-07 12:28:36 +0100

    dfghjkjhgf

---
+++ 6-print_comb3.py
@@ -0,0 +1,6 @@
+# Write a program that prints all possible different combinations of two digits.
+x=input("enter the first digit : ")
+y=input("enter the second digit : ")
+A= str(x,y)
+B= str(y,x)
+print("possible different combinations of two digitis : ", A ,"and " ,B)
