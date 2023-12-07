8907320c8eb0807eb7e870cd056ca3f0a7b6d722
brahimmaji <brahimmaji1@gmail.com>
2023-12-07 22:05:07 +0100

    vvvvv

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+    from add_0 import add
+
+    # Perform the addition and print the result
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
