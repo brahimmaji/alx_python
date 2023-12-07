9e1a3582d3055fff1402ebee0b234ac96a703fe2
brahimmaji <brahimmaji1@gmail.com>
2023-12-07 12:04:51 +0100

    5575555

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,5 @@
 for i in range(100):
-   if i < 100 : 
-    print(f"{i:02d}" + ", "  , end="".format(i))
+   if i < 99 : 
+    print(f"{i:02d}"   , end=" , " .format(i))
    else :
-     print("\n") 
+     print( i,"\n") 
