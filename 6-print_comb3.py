7ab0f868396e2f789cb0e32e738d599f662591c3
brahimmaji <brahimmaji1@gmail.com>
2023-12-07 12:41:44 +0100

    1598753

--- 6-print_comb3.py
+++ 6-print_comb3.py
@@ -1,3 +1,7 @@
 for i in range(10):
     for j in range( i + 1,10 ):
-        print("{:02}".format(i * 10 + j), end=", " if (i != 8 or j != 9) else "\n")
+         if (i != 8 or j != 9) :
+            print("{:02}".format(i * 10 + j), end=", "  )
+      
+         else :
+           print(i*10+j)
