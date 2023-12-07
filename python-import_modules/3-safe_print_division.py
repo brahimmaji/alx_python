503ae26eb77108fc8f3a72facc0cab45253c8f3e
brahimmaji <brahimmaji1@gmail.com>
2023-12-07 22:36:49 +0100

    vblmù

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,9 +1,19 @@
 def safe_print_division(a, b):
     result=a/b
     try:
-         b > 0 
+         b < 0 
             
     except:
         print("Error") 
+    try:
+         a == 0 
+            
+    except:
+        print("Error none")    
+       try:
+         b < 0 and a < 0
+            
+    except:
+        print("Error")      
     finally:
         print("the result of {:d}/{:d} = {:d}".format(a,b,result))    
