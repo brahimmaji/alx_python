64b0b8030ac9318797efd5e70e6bae79ff30b4d2
brahimmaji <brahimmaji1@gmail.com>
2023-12-10 16:46:24 +0100

    args2

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,8 +1,16 @@
-# Write a program that prints the number of and the list of its arguments.
+from sys import argv
+
+def print_arguments():
+    num_args = len(argv) - 1  
+
+    if num_args == 0:
+        print("Number of argument(s): 0.")
+        print(".")
+    else:
+        print(f"Number of argument(s): {num_args}, {'argument' if num_args == 1 else 'arguments'}:")
+        
+        for i in range(1, len(argv)):
+            print(f"{i}: {argv[i]}")
+
 if __name__ == "__main__":
-
-    from args_1 import print_arguments
-
-    result = print_arguments()
-    if result:
-        print(result)
+    print_arguments()
