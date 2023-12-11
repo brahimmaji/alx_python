a94d73f85f0dd88eaead11ae32abfa6f55bbd827
brahimmaji <brahimmaji1@gmail.com>
2023-12-11 16:55:58 +0100

    aaaa

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,14 @@
+def print_matrix_integer(matrix=[]):
+    for i in matrix:
+        print(" ".join("{:d}".format(num) for num in i))
+        
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
