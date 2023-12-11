3419023a877cd026babeaea78e701229f974e0cb
brahimmaji <brahimmaji1@gmail.com>
2023-12-11 15:47:09 +0100

    2. More returns

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,10 @@
-#Write a function that prints a matrix of integers.
+def multiple_returns(sentence):
+    length=len(sentence)
+    first=sentence[0:1]
+    if sentence != "" :
+        print("Length: {:d} - First character: {}".format(length, first))
 
-def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        for y in x:
-            print(y,end="")
-    print()    
+    else:
+        print(None)   
+    return length,first
 
-
-#You can assume that the list only contains integers
-#You are not allowed to cast integers into strings
-#You have to use str.format() to print integers
