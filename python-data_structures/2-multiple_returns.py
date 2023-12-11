111c3c453141ea8caefe331405f6fc9d723c4756
brahimmaji <brahimmaji1@gmail.com>
2023-12-11 16:26:08 +0100

    vh

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,9 +1,10 @@
 def multiple_returns(sentence):
     length=len(sentence)
     first=sentence[0:1]
-    if sentence != "" :
+    if sentence == "" :
+        first= None
         print("Length: {:d} - First character: {}".format(length, first))
+    else :
+        print("Length: {:d} - First character: {} ".format(length,first))   
 
-    else:
-        print(("Length: {:d} - First character:", None).format(length))   
-   # return length,first
+print(multiple_returns(""))
