316c2740421496e717bb82f3dd47c4a8bf6256d4
brahimmaji <brahimmaji1@gmail.com>
2023-12-12 15:12:27 +0100

    AAA

--- python-data_structures/tets.py
+++ python-data_structures/tets.py
@@ -1,2 +1,10 @@
-x = lambda a : a + 10
-print(x(5))
+def fahrenheit(n):
+    return ((float(9)/5)*n + 32)
+def celsius(n):
+    return (float(5)/9)*(n-32)
+
+temperatures = (36.5, 37, 37.5, 38, 39)
+
+map(fahrenheit,temperatures)
+map(celsius,temperatures)
+
--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,23 @@
-# Write a function that computes the square value of all integers of a matrix
 
-#def square_matrix_simple(matrix=[]):
-#matrix is a 2 dimensional array
 def square_matrix_simple(matrix=[]):
     result=[]
     for x in matrix:
         square_row = []
-        square_row =[ y**2 for y in x]
+        square_row.append =[ y**2 for y in x]
         matrix.append(square_row)
     return result
+#def square_matrix_simple(matrix=[]):
+ 
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    squared_matrix = square_matrix_simple(matrix)
+    print(squared_matrix)
+    print(matrix)
+
+
