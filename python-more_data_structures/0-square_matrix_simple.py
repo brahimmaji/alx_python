055811ad7ae011150da2b7be06e264a0e9f88f45
brahimmaji <brahimmaji1@gmail.com>
2023-12-12 11:05:43 +0100

    ccc

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,18 @@
 def square_matrix_simple(matrix=[]):
     matrix=[]
     for x in matrix:
-        for y in x :
-         new_matrix=y**2
-    return new_matrix
+        square_row =[ y**2 for y in x]
+        matrix.append(square_row)
+    return matrix
+
+
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
